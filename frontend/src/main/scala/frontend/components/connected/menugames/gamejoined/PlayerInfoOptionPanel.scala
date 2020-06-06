c30df514a556a503c4dd5773efb668dd9a7bad17
antoine <antoine.doeraene@b12-consulting.com>
2020-06-06 22:41:29 +0200

    comment and typo

--- frontend/src/main/scala/frontend/components/connected/menugames/gamejoined/PlayerInfoOptionPanel.scala
+++ frontend/src/main/scala/frontend/components/connected/menugames/gamejoined/PlayerInfoOptionPanel.scala
@@ -5,17 +5,27 @@ import com.raquo.airstream.eventbus.EventBus
 import com.raquo.laminar.api.L._
 import com.raquo.laminar.nodes.ReactiveHtmlElement
 import frontend.components.Component
-import frontend.components.utils.{ColorPickerWrapper, ToggleButton}
+import frontend.components.utils.laminarutils.reactChildInDiv
 import frontend.components.utils.tailwind.{primaryColour, primaryColourDark}
+import frontend.components.utils.{ColorPickerWrapper, ToggleButton}
 import models.bff.outofgame.PlayerClasses
 import models.bff.outofgame.gameconfig.PlayerStatus.{NotReady, Ready}
 import models.bff.outofgame.gameconfig.{PlayerInfo, PlayerStatus}
 import org.scalajs.dom.html
 import org.scalajs.dom.html.{Element, Select}
-import utils.misc.{Colour, RGBAColour, RGBColour}
-import frontend.components.utils.laminarutils.reactChildInDiv
-import frontend.components.utils.popper.{PlacementOption, PopperElement}
+import utils.misc.{RGBAColour, RGBColour}
 
+/**
+  * This component is displayed in the GameJoined component, and allows the player to select the game configuration
+  * that concerns their entity.
+  *
+  * That means: their class, their colour...
+  *
+  * @param initialPlayerInfo the initial player info as it is currently in the database. This is important because the
+  *                          player can refresh their screen and need to have up to date information
+  * @param playerInfoWriter each time the player makes a change to their character, the new information is sent to the
+  *                         server through this [[com.raquo.airstream.core.Observer]], which ends up in the web socket.
+  */
 final class PlayerInfoOptionPanel private (initialPlayerInfo: PlayerInfo, playerInfoWriter: Observer[PlayerInfo])
     extends Component[html.Element] {
 
@@ -32,6 +42,10 @@ final class PlayerInfoOptionPanel private (initialPlayerInfo: PlayerInfo, player
     changer(info)
   }
 
+  /**
+    * This is the selector for the Player class.
+    * Choosing a different option goes to the playerClassWriter above
+    */
   val classSelector: ReactiveHtmlElement[Select] = select(
     PlayerClasses.allChoices.map(_.toString).map(cls => option(value := cls, cls)),
     inContext(
@@ -43,6 +57,12 @@ final class PlayerInfoOptionPanel private (initialPlayerInfo: PlayerInfo, player
     onMountSet(_ => value := initialPlayerInfo.playerClass.toString)
   )
 
+  /**
+    * Part of the component to chose the colour.
+    *
+    * This is a simple rectangle on the screen, on which you can click. Clicking on it make the Colour picker appear.
+    * Players can then chose a colour they like from the picker.
+    */
   val pickerPositionBus: EventBus[(Double, Double)] = new EventBus
   val $maybePickerPosition: Signal[Option[(Double, Double)]] =
     pickerPositionBus.events.fold(Option.empty[(Double, Double)]) {
@@ -50,7 +70,7 @@ final class PlayerInfoOptionPanel private (initialPlayerInfo: PlayerInfo, player
       case (Some(_), _)   => None
     }
   val colourSelector: ReactiveHtmlElement[html.Div] = div(
-    "Chose a color: ",
+    "Choose a color: ",
     div(
       height := "30px",
       width := "50px",
