10b11ba7c9de3beb0c39b13ba1010e65c13ba283
antoine <antoine.doeraene@b12-consulting.com>
2020-12-29 15:35:43 +0100

    images for markers, and adapted GameState class

---
+++ shared/src/main/scala/gamelogic/gameextras/GameMarker.scala
@@ -0,0 +1,41 @@
+package gamelogic.gameextras
+
+/**
+  * A [[GameMarker]] is a token that players can put in the game in order to improve
+  * communication via some "markers".
+  * 
+  * There are a fixed, hardcoded number of markers that player can use, and they are all
+  * implemented below. Markers have no game semantic and, even though we encode them in the
+  * [[gamelogic.gamestate.GameState]] for simplicity, IAs will never use them.
+  * 
+  * Markers can be put either in a fixed position in the game, or they can be assigned
+  * to a particular entity (friend or foe).
+  */
+sealed trait GameMarker {
+  def id: Int
+}
+
+object GameMarker {
+
+  case object Cross extends GameMarker {
+    def id: Int = 1
+  }
+  case object Lozenge extends GameMarker {
+    def id: Int = 2
+  }
+  case object Moon extends GameMarker {
+    def id: Int = 3
+  }
+  case object Square extends GameMarker {
+    def id: Int = 4
+  }
+  case object Star extends GameMarker {
+    def id: Int = 5
+  }
+  case object Triangle extends GameMarker {
+    def id: Int = 6
+  }
+
+  
+
+}
---
+++ shared/src/main/scala/gamelogic/gameextras/GameMarkerInfo.scala
@@ -0,0 +1,42 @@
+package gamelogic.gameextras
+
+import gamelogic.gamestate.GameState
+import gamelogic.physics.Complex
+import gamelogic.entities.Entity
+
+/**
+  * A [[GameMarkerInfo]] remembers all there is to know for a certain [[GameMarker]].
+  * 
+  * The most important things are what marker is represented, and where it should be
+  * at a certain time.
+  */
+sealed trait GameMarkerInfo {
+  def marker: GameMarker
+
+  /**
+    * Returns the game position of the [[GameMarker]] at the specified time and for
+    * the specified [[GameState]].
+    */
+  def maybePosition(gameState: GameState, currentTime: Long): Option[Complex]
+}
+
+object GameMarkerInfo {
+
+  /**
+    * Fixed marker which stays on the ground, and never moves.
+    */
+  case class FixedGameMarker(marker: GameMarker, gamePosition: Complex) extends GameMarkerInfo {
+    def maybePosition(gameState: GameState, currentTime: Long): Option[Complex] = Some(gamePosition)
+  }
+
+  /**
+    * [[GameMarker]] attached to a certain [[Entity]] in the game.
+    * In practice, this entity should always be a [[gamelogic.entities.Body]], since
+    * we at least need to have a position.
+    */
+  case class GameMarkerOnEntity(marker: GameMarker, entityId: Entity.Id) extends GameMarkerInfo {
+    def maybePosition(gameState: GameState, currentTime: Long): Option[Complex] = 
+      gameState.movingBodyEntityById(entityId).map(_.currentPosition(currentTime))
+  }
+
+}
